1fccdbdfc1dcf637f383433d72fbdcadd15d2c6d
Soumya Ghosh <soumyaidmail@gmail.com>
2024-12-24 14:46:33 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,59 @@
+import { useState } from "react";
 import "./App.css";
 import CurrencyBox from "./components/CurrencyBox";
+import useGetCurrency from "./hooks/useGetCurrency";
 
 function App() {
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [amount, setAmount] = useState(0);
+  const [convertedAmmount, setConvertedAmmount] = useState(0);
+
+  const currencyInfo = useGetCurrency(from);
+
+  const options = Object.keys(currencyInfo);
+
+  const convert = () => {
+    setConvertedAmmount(amount * currencyInfo[to]);
+  };
+  function onValueChanged(newValue) {
+    console.log("came", newValue);
+    setAmount(newValue);
+    convert();
+  }
+  function onToCurrencyChange(updatedToCountry) {
+    setTo(updatedToCountry);
+    convert();
+  }
+  function onFromCurrencyChange(updatedFromCountry) {
+    setFrom(updatedFromCountry);
+    convert();
+  }
+
   return (
     <>
-      <div className="bg-slate-800 h-lvh flex justify-center items-center ">
-        <div className="flex flex-col justify-center items-center gap-y-4 bg-transparent p-6 border border-gray-800 rounded-lg shadow bg-slate-950 pt-12 pb-12 hover:bg-slate-900">
-          <h1 className="text-white text-xl ">Currency Converter</h1>
-          <CurrencyBox />
-          <button className="bg-slate-600 hover:bg-slate-500 text-white py-2 px-4 border border-slate-500 hover:border-transparent rounded">
-            Convert
-          </button>
-          <CurrencyBox />
+      <div className="bg-slate-800 h-lvh flex justify-center items-center">
+        <div className="bg-slate-950 flex flex-col justify-center items-center gap-y-4 p-6 border border-gray-800 rounded-lg shadow pt-12 pb-12 hover:bg-slate-900">
+          <h1 className="text-white text-2xl ">Currency Converter</h1>
+          <CurrencyBox
+            fromCountry={from}
+            toCountry={to}
+            ammount={amount}
+            convertedAmmount={convertedAmmount}
+            currencyOptions={options}
+            isFrom={true}
+            onValueChanged={(amount) => onValueChanged(amount)}
+            onFromChanged={(country) => onFromCurrencyChange(country)}
+          />
+          <CurrencyBox
+            fromCountry={from}
+            toCountry={to}
+            ammount={amount}
+            convertedAmmount={convertedAmmount}
+            currencyOptions={options}
+            isFrom={false}
+            onToChanged={(country) => onToCurrencyChange(country)}
+          />
         </div>
       </div>
     </>
--- src/components/CurrencyBox.jsx
+++ src/components/CurrencyBox.jsx
@@ -1,17 +1,41 @@
-CurrencyBox.propTypes = {};
-
-function CurrencyBox() {
+function CurrencyBox({
+  fromCountry,
+  toCountry,
+  amount,
+  convertedAmmount,
+  currencyOptions,
+  isFrom,
+  onValueChanged,
+  onFromChanged,
+  onToChanged
+}) {
   return (
     <>
       <div className="bg-slate-300 h-auto w-auto p-5 flex flex-row justify-center items-center gap-x-5 rounded-md shadow-sm">
         <input
           type="number"
           className="h-8 w-96 p-5 rounded-sm"
-          placeholder="INR"
+          placeholder="0"
+          disabled={!isFrom}
+          value={isFrom ? amount : convertedAmmount}
+          onChange={(e) => onValueChanged && onValueChanged(Number(e.target.value))}
         ></input>
-        <select>
-          <option value="INR">INR</option>
-          <option value="USD">USD</option>
+        <select className="p-2 rounded-md"
+         onChange={onFromChanged ? (e) => onFromChanged && onFromChanged(e.target.value) : (e) => onToChanged && onToChanged(e.target.value)}>
+          {currencyOptions.map((currency) => (
+            <option
+              key={currency}
+              value={currency}
+              selected={
+                (fromCountry == currency && isFrom) ||
+                (toCountry == currency && !isFrom)
+                  ? true
+                  : false
+              }
+            >
+              {currency}
+            </option>
+          ))}
         </select>
       </div>
     </>
